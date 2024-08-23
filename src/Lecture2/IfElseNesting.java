1845276b7ca07b4efb815833c4cbadca1678d5d6
Jatin Kumar <jatinkumar8125@gmail.com>
2024-08-23 14:34:37 +0530

    learned scanner and implemented it in the previous code.

--- src/Lecture2/IfElseNesting.java
+++ src/Lecture2/IfElseNesting.java
@@ -7,10 +7,19 @@ public class IfElseNesting {
         //lets make a program to tell whether the user is eligible to vote or not
         //in order to be eligible to vote he/she has to be over 18 and have an adhar card
 
-        int age = 18;
-        boolean hasAdhar = true;
+//        int age = 18;
+//        boolean hasAdhar = true;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter your Age: ");
+        int age = scan.nextInt();
+        System.out.println("Please tell if you have an adhar card or not [Y/N]");
+        scan.nextLine();
+        String hasAdhar = scan.nextLine();
+        scan.close();
+
         if (age >= 18) {
-            if (hasAdhar == true) {
+            if (hasAdhar.equalsIgnoreCase("y")) {
                 System.out.println("Congrats!! You are eligible to vote!");
             } else {
                 System.out.println("Sorry you don't have a valid Adhar card.");
--- src/Lecture2/IfelseifLadder.java
+++ src/Lecture2/IfelseifLadder.java
@@ -1,27 +1,42 @@
 package Lecture2;
 
+import java.util.Scanner;
 public class IfelseifLadder {
     public static void main(String[] args) {
         //we are going to make the same program that we made using the nested if else here
         // the reason why we prefer this is because it allows us to process only one condition at a time
         //which makes the code easier to understand
 
-        int age = 17;
-        boolean hasAdhar = true;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter your Age: ");
+        int age = scan.nextInt();
+        System.out.println("Please tell if you have an adhar card or not [Y/N]");
+        scan.nextLine();
+        String hasAdhar = scan.nextLine();
+
+//        if (age > 18) {
+//            System.out.println("18+");
+//        } else if (age == 18) {
+//            System.out.println("18");
+//        } else if (age < 18){
+//            System.out.println("18-");
+//        } else {
+//            System.out.println("bugged");
+//        }
 
         // this example uses conditional and operational logic to reduce the nesting
-        if (age >= 18 && hasAdhar == true){
+        if (age >= 18 && (hasAdhar.equalsIgnoreCase("y"))){
             System.out.println("Congrats you are eligible to vote!");
         } else {
             System.out.println("Sorry you are not allowed to vote");
         }
 
         //this is just make an example of ladder
-        if (age >= 18 && hasAdhar == true) {
+        if (age >= 18 && hasAdhar.equalsIgnoreCase("y")) {
             System.out.println("eligible");
-        } else if (age >= 18 && hasAdhar == false) {
+        } else if (age >= 18 && hasAdhar.equalsIgnoreCase("n")) {
             System.out.println("you do not have a valid adhar card");
-        } else if (age < 18 && hasAdhar == true) {
+        } else if (age < 18 && hasAdhar.equalsIgnoreCase("y")) {
             System.out.println("you are not above the age of 18");
         } else {
             System.out.println("you do not have an adhra card and you are also not above 18");
